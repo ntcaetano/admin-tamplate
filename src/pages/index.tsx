9ec3f87c2f6c6e402c4c92434afe886027788d11
Natanael Caetano <natacaet@gmail.com>
2022-03-14 11:38:22 -0300

    update

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    <div className={`
+
+    h- screen
+     bg-gradient-to-r from-green-500 to-blue-500
+      
+
+    `}>
+      
+    </div>
+  )
+}
