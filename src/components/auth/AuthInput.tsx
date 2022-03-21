81757344eda7f96a9f049b513c56403f9c822813
Natanael Caetano <natacaet@gmail.com>
2022-03-21 14:50:24 -0300

    update

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -9,13 +9,18 @@ interface AuthInputProsps {
 
 export default function AuthInput(props: AuthInputProsps) {
     return(
-        <div className="flex flex-col">
+        <div className="flex flex-col mt-4">
             <label>{props.label}</label>
             <input
                 type={props.tipo ?? 'text'}
                 value={props.valor}
                 onChange={e => props.valorMudou?.(e.target.value)}
                 required={props.obrigatorio}
+                className={`
+                    bg-gray-200 px-4 py-3 rounded-lg mt-2
+                    border focus:border-blue-500 focus:bg-white
+                    focus:outline-none
+                `}
              />
         </div>
     )
--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,9 +1,6 @@
 import Layout from "../components/template/Layout";
-import { useTema } from "../hooks/useTema";
 
 export default function Notificacoes() {
-  const { AlternarTema, tema } = useTema();
-
   return (
     <Layout titulo="Notificações"
       subtitulo="Aqui você irá gerenciar as suas notificações">
